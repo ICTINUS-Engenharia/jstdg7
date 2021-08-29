bcc96e203d38ac25db2190b012f56575ac97c65f
ICTINUS Serviços de Engenharia e Automação <luciano@ictinus.com.br>
2021-08-29 17:36:28 +0000

    Luciano

--- ch01/Teste.js
+++ ch01/Teste.js
@@ -1,2 +1,5 @@
-let d = new Date(2010, 0, 1);   // January 1, 2010, (Pacific time)
-d.valueOf()                     // => 1262332800000
+[1,2,3].toString()                  // => "1,2,3"
+(function(x) { f(x); }).toString()  // => "function(x) { f(x); }"
+/\d+/g.toString()                   // => "/\\d+/g"
+let d = new Date(2020,0,1);
+d.toString()  // => "Wed Jan 01 2020 00:00:00 GMT-0800 (Pacific Standard Time)"
