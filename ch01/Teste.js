3027f8fe8779351456cf1fb08a267d2ce686469b
ICTINUS-Engenharia <luciano@ictinus.com.br>
2021-09-20 21:48:29 -0300

    Teste

--- ch01/Teste.js
+++ ch01/Teste.js
@@ -1,2 +1,6 @@
 let a = "Luciano Cardoso Vasconcelos";
 console.log(a);
+
+let message = "hello";
+let i = 0, j = 0, k = 0;
+let x = 2, y = x * x; // Initializers can use previously declared variables
