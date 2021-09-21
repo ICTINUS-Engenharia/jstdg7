15b20a3aed6423a9f9ea2d19c1f73036a3a4d6c7
ICTINUS-Engenharia <luciano@ictinus.com.br>
2021-09-20 21:55:04 -0300

    Teste

--- ch01/Teste.js
+++ ch01/Teste.js
@@ -4,3 +4,7 @@ console.log(a);
 let message = "hello";
 let i = 0, j = 0, k = 0;
 let x = 2, y = x * x; // Initializers can use previously declared variables
+
+const H0 = 74;         // Hubble constant (km/s/Mpc)
+const C = 299792.458;  // Speed of light in a vacuum (km/s)
+const AU = 1.496E8;    // Astronomical Unit: distance to the sun (km)
